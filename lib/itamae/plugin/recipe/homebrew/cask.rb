c1570a63984075b67c82a4cb6bed479c11f47b45
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2017-01-15 15:11:52 +0900

    Remove unnecessary var

--- lib/itamae/plugin/recipe/homebrew/cask.rb
+++ lib/itamae/plugin/recipe/homebrew/cask.rb
@@ -6,9 +6,7 @@ execute 'Install Homebrew Cask' do
   not_if "brew tap | grep '^#{repo}$'"
 end
 
-# Install apps
-install_apps = node['brew']['install_apps']
-install_apps.each do |app|
+node['brew']['install_apps'].each do |app|
   execute "Install application: #{app}" do
     command "brew cask install #{app}"
     not_if "brew cask list | grep '^#{app}$'"
