12d149bf21ff6d06bdf31e66c337dd4e58d5da44
Haru Akebono <haru.akebono.11@gmail.com>
2020-03-06 22:11:45 +0900

    support bash installer

--- lib/itamae/plugin/recipe/homebrew/install/linux.rb
+++
@@ -1,5 +0,0 @@
-BREW_INSTALL_URL = 'https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh'.freeze
-execute "Install Homebrew" do
-  command "sh -c \"$(curl -fsSL #{BREW_INSTALL_URL})\""
-  not_if "test $(which brew)"
-end
