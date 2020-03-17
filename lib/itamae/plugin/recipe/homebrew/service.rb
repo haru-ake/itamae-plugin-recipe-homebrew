828469bd158eb7ac18fba398f61e74eed26698e1
Haru Akebono <haru.akebono.11@gmail.com>
2020-03-17 22:22:38 +0900

    support `brew service`

---
+++ lib/itamae/plugin/recipe/homebrew/service.rb
@@ -0,0 +1,10 @@
+if node[:platform] == 'darwin'
+  (node['brew']['services'] || []).each do |service|
+    execute "Start service: #{service}" do
+      command "brew services start #{service}"
+      not_if "brew services | grep '^#{service} ' | grep ' started '"
+    end
+  end
+else
+  Itamae.logger.info("#{node[:platform]} is not support service")
+end
